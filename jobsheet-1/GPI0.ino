8a88f7f75863d6410946ccafd9ea7f599c8265de
dionysiusyudhistira <121749328+dionysiusyudhistira@users.noreply.github.com>
2023-01-05 21:14:30 +0700

    Update GPI0.ino

--- jobsheet-1/GPI0.ino
+++ jobsheet-1/GPI0.ino
@@ -1,10 +1,10 @@
 // set pin numbers
 const int buttonPin = 4; // the number of the pushbutton pin
 const int ledPin = 5; // the number of the LED pin
-// variable for storing the pushbutton status
+// variable for storing the pushbutton status 
 int buttonState = 0;
 void setup() {
- Serial.begin(115200);
+ Serial.begin(115200); 
  // initialize the pushbutton pin as an input
  pinMode(buttonPin, INPUT);
  // initialize the LED pin as an output
